6ed6d97ab4b81df480f4aea8b35a8018319bea35
fredo09 <fred_15@outlook.com>
2025-03-10 21:32:40 -0600

    add new interface for movie detail

--- infraestructure/interfaces/movies-db/moviedb.interface.ts
+++ infraestructure/interfaces/movies-db/moviedb.interface.ts
@@ -7,3 +7,11 @@ export interface Movie {
     poster: string;
     backdrop: string;
 }
+
+export interface MovieDetailComplete extends Movie {
+    genres: string[];
+    duration: number;
+    budget: number;
+    originalTitle: string;
+    productionCompanies: string[];
+}
